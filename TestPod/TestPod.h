24c7775ebeeb9c3333e668f5b8c1f68b1142c372
marwamelfeky <marwamelfeky@gmail.com>
2023-08-01 09:19:27 +0300

    Pod Files

---
+++ TestPod/TestPod.h
@@ -0,0 +1,18 @@
+//
+//  TestPod.h
+//  TestPod
+//
+//  Created by Marwa Elfeky on 31/07/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestPod.
+FOUNDATION_EXPORT double TestPodVersionNumber;
+
+//! Project version string for TestPod.
+FOUNDATION_EXPORT const unsigned char TestPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestPod/PublicHeader.h>
+
+
