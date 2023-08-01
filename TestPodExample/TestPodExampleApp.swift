bb950bea997f8529a50fe8b163c155083693eb29
marwamelfeky <marwamelfeky@gmail.com>
2023-08-01 09:18:23 +0300

    Example

---
+++ TestPodExample/TestPodExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestPodExampleApp.swift
+//  TestPodExample
+//
+//  Created by Marwa Elfeky on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestPodExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
